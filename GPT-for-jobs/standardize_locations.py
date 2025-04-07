6825bebb22d87b1c9fe828187b50f1197c8423da
Ruihao Qiu <qiuruihao@gmail.com>
2025-04-07 23:54:34 +0200

    Create standardize_locations.py

---
+++ GPT-for-jobs/standardize_locations.py
@@ -0,0 +1,43 @@
+from typing import List
+from pydantic import BaseModel
+from enum import Enum
+
+from openai import AsyncOpenAI
+import instructor
+
+aclient = instructor.patch(AsyncOpenAI())
+
+class StandardLocation(BaseModel):
+    city: str
+    county: str
+    country: str
+
+def make_prompt():
+    """
+    here is just an example, make you own prompt and test it.
+    """
+    prompt = (
+        "You are a geographic expert. "
+        "You will be provided a raw text that might include location information. "
+        "And you should give back a list of standard locations including city, county and country. "
+        "If there is no city or county, keep it as None. "
+    )
+    return prompt
+
+async def standardize_location(
+    raw_location: str
+) -> List[StandardLocation]:
+    prompt = make_prompt()
+    response = await aclient.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": prompt,
+            },
+            {"role": "user", "content": raw_location},
+        ],
+        max_retries=5,
+        response_format=List[StandardLocation]
+    )
+    return response
