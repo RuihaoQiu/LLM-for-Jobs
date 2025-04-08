7ce08bdedcb3292d20e9df6a5c9ac6016ba9a3ad
Ruihao Qiu <qiuruihao@gmail.com>
2025-04-08 23:46:31 +0200

    reformat files

--- GPT-for-jobs/generate_titles.py
+++ GPT-for-jobs/generate_titles.py
@@ -19,21 +19,12 @@ async def generate_title(text: str) -> str:
         You will be provided with a job description, and your task is to create a proper title for it.
         The title should be simple and clear, neither too general nor too specific, at maximum 4 words."""
     messages = [
-                    {
-                        "role": "system",
-                        "content": prompt
-                    },
-                    {
-                        "role": "user",
-                        "content": text
-                    }
-                ]
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": text},
+    ]
 
     try:
-        response = await aclient.chat.completions.create(
-            model=model,
-            messages=messages
-        )
+        response = await aclient.chat.completions.create(model=model, messages=messages)
         output = response.choices[0].message.content
         return output
     except Exception as e:
@@ -46,21 +37,12 @@ async def select_title(text: str, titles: List[str]) -> str:
         You will be provided with a job description, and your task is to select a proper title from the provided list: {titles}.
         If there is no proper title, just return None."""
     messages = [
-                    {
-                        "role": "system",
-                        "content": prompt
-                    },
-                    {
-                        "role": "user",
-                        "content": text
-                    }
-                ]
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": text},
+    ]
 
     try:
-        response = await aclient.chat.completions.create(
-            model=model,
-            messages=messages
-        )
+        response = await aclient.chat.completions.create(model=model, messages=messages)
         output = response.choices[0].message.content
         return output
     except Exception as e:
