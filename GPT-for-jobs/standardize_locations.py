6c50952284ba807991a7acf17c43f5ea7608d79a
Ruihao Qiu <qiuruihao@gmail.com>
2025-04-25 23:26:49 +0200

    update with minor issues

--- GPT-for-jobs/standardize_locations.py
+++ GPT-for-jobs/standardize_locations.py
@@ -46,6 +46,6 @@ async def standardize_locations(locations: list[str]) -> list[StandardLocation]:
 
 
 if __name__ == "__main__":
-    raw_location = "Beijing, Beijing, CN"
-    standardized_location = asyncio.run(standardize_location(raw_location=raw_location))
+    location = "Beijing, Beijing, CN"
+    standardized_location = asyncio.run(standardize_location(raw_location=location))
     print(standardized_location)
