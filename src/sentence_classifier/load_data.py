5ec2a6c120c4f71c53d4f27a3b5eecec588ae434
Ruihao Qiu <qiuruihao@gmail.com>
2025-05-12 23:45:47 +0200

    rewrite load_data

--- src/sentence_classifier/load_data.py
+++ src/sentence_classifier/load_data.py
@@ -1,14 +1,16 @@
 import pandas as pd
+from pathlib import Path
+from typing import List
 from src.config import data_folder
 
+def get_csv_files(folder: Path) -> List[Path]:
+    return [file for file in folder.iterdir() if file.is_file() and file.suffix == ".csv"]
+
+def read_csv_files(files: List[Path]) -> List[pd.DataFrame]:
+    return [pd.read_csv(file) for file in files]
 
 def load_raw_data() -> pd.DataFrame:
-    dfs = []
     raw_data_path = data_folder / "raw_data"
-    for file in raw_data_path.iterdir():
-        if file.is_file() and file.suffix == ".csv":
-            file_path = raw_data_path / file
-            df_batch = pd.read_csv(file_path)
-            dfs.append(df_batch)
-    df = pd.concat(dfs, ignore_index=True)
-    return df
+    csv_files = get_csv_files(raw_data_path)
+    dataframes = read_csv_files(csv_files)
+    return pd.concat(dataframes, ignore_index=True)
