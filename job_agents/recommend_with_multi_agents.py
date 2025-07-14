1ac5a3215c49e3dad1392db3b3f0242840416152
Ruihao Qiu <qiuruihao@gmail.com>
2025-07-14 15:09:02 +0800

    Update recommend_with_multi_agents.py

--- job_agents/recommend_with_multi_agents.py
+++ job_agents/recommend_with_multi_agents.py
@@ -1,10 +1,11 @@
 import asyncio
 from agents import Agent, Runner
 
-async def main():
-    agent = Agent(name="Assistant", instructions="You are a helpful assistant")
 
-    result = await Runner.run(agent, "Write a haiku about recursion in programming.")
+async def main():
+    agent = Agent(name="Assistant", instructions="You are an expert of job and skill catelogue.")
+
+    result = await Runner.run(agent, "Recommend 5 most relevant titles from ONet.")
     print(result.final_output)
     # Code within the code,
     # Functions calling themselves,
